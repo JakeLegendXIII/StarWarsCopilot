794737fb04a24808d27c06983417c0095ef4b528
Jake Kirkbride <jakelegendxiii@gmail.com>
2026-01-14 15:18:03 -0500

    Add MCP functionality

---
+++ StarWarsCopilot/MCPServerOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace StarWarsCopilot;
+
+public static class MCPServerOptions
+{
+	public static string Name => "StarWarsMCPServer";
+	public static string Command => "dotnet";
+	public static List<string> Arguments => [
+		"run",
+		"--project",
+		"C:\\Projects\\CodeMash2026\\StarWarsCopilot\\StarWarsMCPServer\\StarWarsMCPServer.csproj"
+	];
+}
